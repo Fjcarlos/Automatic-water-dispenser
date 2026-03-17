927bec91c7e6dbf877fa0cf698b7b3b42647ea00
Fjcarlos <fjncarlos@gmail.com>
2026-03-17 23:17:02 +0800

    Refactor water dispenser code for clarity and efficiency

--- Automatic-water-dispenser.ino
+++ Automatic-water-dispenser.ino
@@ -1,55 +1,81 @@
-float distance2;
 float distance1;
-void setup(){
- Serial.begin(9600);
- pinMode(2, OUTPUT);//motor
- pinMode(3, OUTPUT);//buzzer
- pinMode(5,OUTPUT);
- pinMode(10,OUTPUT);
+float distance2;
+
+unsigned long previousBuzzerTime = 0;
+bool buzzerState = false;
+
+void setup() {
+  Serial.begin(9600);
+
+  pinMode(2, OUTPUT); // pump
+  pinMode(3, OUTPUT); // buzzer
+  pinMode(5, OUTPUT); // trigger2
+  pinMode(10, OUTPUT); // trigger1
 }
-void loop()
-{
+
+void loop() {
+
   ultrasonic1();
   Serial.println(distance1);
-  while(distance1<5){
-    ultrasonic1();
+
+  // Check if cup is present
+  if (distance1 < 5) {
+
     ultrasonic2();
     Serial.println(distance2);
-    if(distance2<4){
-    while(distance2<4){
-    ultrasonic2();
-    Serial.println(distance2);
-    digitalWrite(2, LOW);
-    digitalWrite(3, HIGH);
-    delay(1000);
-    digitalWrite(3, LOW);
-    delay(1000);
-     }
-    }else{
-      digitalWrite(3,LOW);
-      digitalWrite(2, HIGH);
+
+    // Check liquid level
+    if (distance2 < 4) {
+
+      digitalWrite(2, LOW); // stop pump
+
+      // Non-blocking buzzer toggle
+      if (millis() - previousBuzzerTime >= 1000) {
+        previousBuzzerTime = millis();
+        buzzerState = !buzzerState;
+        digitalWrite(3, buzzerState);
+      }
+
+    } 
+    else {
+
+      digitalWrite(3, LOW); // buzzer off
+      digitalWrite(2, HIGH); // pump on
+
     }
+
+  } 
+  else {
+
+    digitalWrite(3, LOW);
+    digitalWrite(2, LOW);
+
   }
-  digitalWrite(3,LOW);
-  digitalWrite(2,LOW);
+
 }
-void ultrasonic1(){
+
+void ultrasonic1() {
+
   digitalWrite(10, LOW);
   delayMicroseconds(5);
   digitalWrite(10, HIGH);
   delayMicroseconds(10);
   digitalWrite(10, LOW);
+
   distance1 = pulseIn(11, HIGH);
-  distance1 = distance1*0.0176;
-  delay(50);
+  distance1 = distance1 * 0.0176;
+
 }
-void ultrasonic2(){
+
+void ultrasonic2() {
+
   digitalWrite(5, LOW);
   delayMicroseconds(5);
   digitalWrite(5, HIGH);
   delayMicroseconds(10);
   digitalWrite(5, LOW);
+
   distance2 = pulseIn(6, HIGH);
-  distance2 = distance2*0.0176;
-  delay(50);
+  distance2 = distance2 * 0.0176;
+
 }
